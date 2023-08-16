1f9269bfce6e050bd4258df42c81e7f7aac9ee38
Karen Rodriguez <kyrodriguez@gmail.com>
2023-08-16 15:27:07 -0500

    first commit

---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import {RouterLink} from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <div>
+            <RouterLink :to="{name:'inicio'}">
+                Volver
+            </RouterLink>
+        </div>
+        <h1 class="text-4xl font-extrabold text-slate-500">Agregar cliente</h1>
+    </div>
+</template>
